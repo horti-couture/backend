25f455fb9fdace01698b018900dff5879a774f95
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-03-11 15:16:46 +0200

    Updated to https://luxcare.github.io

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post("/initialize-payment", async (req, res) => {
 
     try {
         const response = await axios.post(
-            "https:// api.paystack.co/transaction/initialize",
+            "https://api.paystack.co/transaction/initialize",
             { email, amount: amount * 100, currency: "ZAR" }, // Convert amount to kobo
             { headers: { Authorization: `Bearer ${PAYSTACK_SECRET_KEY}` } }
         );
